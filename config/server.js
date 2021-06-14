5a777ab1f1be3be74cec96dea58c31b92330ef4a
aayush <aayush.iosdeveloper@gmail.com>
2021-06-14 12:02:25 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '79db7774ccace228ad0bafabe760e48c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '66f66110-bef5-49d6-8a46-5151d42ce91e'
+};
